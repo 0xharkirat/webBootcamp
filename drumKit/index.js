2bbf0891c3532d9622d78e2e8d93c623461f7c33
harkirat2309 <sandhukirat23@gmail.com>
2022-09-09 18:07:20 +1000

    add sounds

--- drumKit/index.js
+++ drumKit/index.js
@@ -1,12 +1,49 @@
+let noOfDrumButtons = document.querySelectorAll(".drum").length;
 
-let noOfDrumButtons = document.querySelectorAll(".drum").length
-
-for (let i = 0; i < noOfDrumButtons; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+for (let i = 0; i < noOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
 
+function handleClick() {
+  let buttonInnerHTML = this.innerHTML;
+  switch (buttonInnerHTML) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
 
-function handleClick(){
-    let audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+        console.log(buttonInnerHTML);
+      break;
+  }
 }
